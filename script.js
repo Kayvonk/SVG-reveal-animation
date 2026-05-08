ca6d7882b23e622269e429c671c50494c2875215
Kayvon Kazemini <kayvonk@gmail.com>
2026-05-08 12:03:24 -0600

    further simplified

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const overlay = document.getElementById('overlay');
 const circle  = document.getElementById('rc0');
 
-circle.setAttribute('cx', innerWidth / 2);
-circle.setAttribute('cy', innerHeight / 2);
-document.getElementById('reveal-bg').setAttribute('width', innerWidth);
-document.getElementById('reveal-bg').setAttribute('height', innerHeight);
 
 circle.addEventListener('animationend', () => {
   overlay.style.opacity = '0';
-  overlay.addEventListener('transitionend', () => overlay.remove(), { once: true });
+  overlay.remove();
 });
